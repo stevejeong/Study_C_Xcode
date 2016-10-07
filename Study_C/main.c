4a94c64da6792e752f519074bd75cff195ddc1d5
stevejeong <stevej78@naver.com>
2016-10-07 23:45:47 +0900

    Initial Commit

---
+++ Study_C/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Study_C
+//
+//  Created by haeyoonpapa on 2016. 10. 7..
+//  Copyright © 2016년 haeyoonpapa. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
